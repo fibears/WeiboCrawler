8d8fc4107a09b84f06c97ab120d94db904c28ed6
fibears <fibears33@sina.com>
2016-05-06 17:08:16 +0800

    Create

---
+++ Weibo/Weibo/items.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class WeiboItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    Content = scrapy.Field()
+    UID = scrapy.Field()
+    Repost = scrapy.Field()
+    Comment = scrapy.Field()
+    Like = scrapy.Field()
+    PostTime = scrapy.Field()
+
+class UserItem(scrapy.Item):
+    """docstring for UserItem"""
+    UID = scrapy.Field()
+    Name = scrapy.Field()
+    FansNum = scrapy.Field()
+    FollowerNum = scrapy.Field()
+    Fans = scrapy.Field()
+    Follower = scrapy.Field()
+
+
